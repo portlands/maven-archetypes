dd961a93ae42ff7bb8b7d94ec8d1d4c8ad96569c
Tobias Sasse <tobi@byte23.de>
2017-11-01 17:24:25 +0100

    modules

--- html5-simple/src/main/resources/archetype-resources/src/app.js
+++ html5-simple/src/main/resources/archetype-resources/src/app.js
@@ -1,2 +1,9 @@
-const main = document.querySelector("main");
-main.innerHTML = "<h1>Hello World!</h1>";
+class App {
+
+  constructor() {
+	const main = document.querySelector("main");
+	main.innerHTML = "<h1>Hello World!</h1>";
+  }
+}
+
+document.app = new App();
